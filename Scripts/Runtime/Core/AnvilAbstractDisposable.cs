c3cb2bc424ccec82addd47dda012464690154bcf
Jon Keon <jon.keon@gmail.com>
2020-01-28 20:49:10 -0500

    Adding AnvilAbstractDisposable

---
+++ Scripts/Runtime/Core/AnvilAbstractDisposable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Anvil.CSharp.Core
+{
+    public abstract class AnvilAbstractDisposable : IDisposable
+    {
+        public bool IsDisposed { get; private set; }
+        public void Dispose()
+        {
+            if (IsDisposed)
+            {
+                return;
+            }
+
+            IsDisposed = true;
+            DisposeSelf();
+        }
+
+        protected virtual void DisposeSelf()
+        {
+            
+        }
+    } 
+}
+
