786436c32c7a9cc089f3100985116d7615876561
Jon Keon <jon.keon@gmail.com>
2020-04-11 13:46:22 -0400

    Implemented the AbstractAnvilApplication and IAnvilApplication

---
+++ Scripts/Runtime/Core/IAnvilApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Anvil.CSharp.Core
+{
+    /// <summary>
+    /// Interface for the entry point for an application.
+    /// </summary>
+    public interface IAnvilApplication: IDisposable 
+    {
+        /// <summary>
+        /// The initialize method to setup the application and begin.
+        /// </summary>
+        void Init();
+    }
+}
+
