8071a38744005ad5df54a1ebf6cee7c82854184b
Jon Keon <jon.keon@gmail.com>
2020-04-12 12:49:50 -0400

    Formatting

--- Scripts/Runtime/Core/IAnvilApplication.cs
+++ Scripts/Runtime/Core/IAnvilApplication.cs
@@ -5,7 +5,7 @@ namespace Anvil.CSharp.Core
     /// <summary>
     /// Interface for the entry point for an application.
     /// </summary>
-    public interface IAnvilApplication: IDisposable 
+    public interface IAnvilApplication : IDisposable 
     {
         /// <summary>
         /// The initialize method to setup the application and begin.
