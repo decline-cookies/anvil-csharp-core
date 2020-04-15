529b2fdf108b2faf612a870f5d11e69d5aa4c0da
Jon Keon <jon.keon@gmail.com>
2020-04-15 12:36:16 -0400

    WIP

---
+++ Scripts/Runtime/Content/AbstractContentController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Anvil.CSharp.Content;
+using Anvil.CSharp.Core;
+
+namespace Anvil.CSharp.Content
+{
+    public abstract class AbstractContentController<T> : AbstractContentController where T : IContent
+    {
+        public new T Content => (T)base.Content;
+
+        protected AbstractContentController(string contentGroupID, string contentLoadingID)
+            : base(contentGroupID, contentLoadingID) { }
+    }
+
+    public abstract class AbstractContentController : AbstractAnvilDisposable
+    {
+        public event Action OnPlayInComplete;
+        public event Action OnPlayOutComplete;
+        public event Action OnLoadComplete;
+
+        public event Action OnClear;
+
+
+        public readonly string ContentGroupID;
+        public readonly string ContentLoadingID;
+
+        public IContent Content { get; private set; }
+        public AbstractContentGroup ContentGroup { get; internal set; }
+        public bool IsContentControllerDisposing { get; private set; }
+
+        protected AbstractContentController(string contentGroupID, string contentLoadingID)
+        {
+            ContentGroupID = contentGroupID;
+            ContentLoadingID = contentLoadingID;
+            //TODO: Handle overrides for additional loading dependency settings.
+        }
+
+        protected override void DisposeSelf()
+        {
+            if (IsContentControllerDisposing)
+            {
+                return;
+            }
+            IsContentControllerDisposing = true;
+            
+            
+            OnPlayInComplete = null;
+            OnPlayOutComplete = null;
+            OnLoadComplete = null;
+            OnClear = null;
+
+            if (Content != null && !Content.IsContentDisposing)
+            {
+                Content.Dispose();
+                Content = null;
+            }
+            
+            base.DisposeSelf();
+        }
+
+        public virtual void Load()
+        {
+            
+        }
+
+        
+
+        internal void InternalInitAfterLoadComplete()
+        {
+            InitAfterLoadComplete();
+        }
+
+        protected abstract void InitAfterLoadComplete();
+
+        internal void InternalPlayIn()
+        {
+            PlayIn();
+        }
+
+        protected abstract void PlayIn();
+        
+        protected virtual void PlayInComplete()
+        {
+            OnPlayInComplete?.Invoke();
+        }
+
+        internal void InternalInitAfterPlayInComplete()
+        {
+            InitAfterPlayInComplete();
+        }
+
+        protected abstract void InitAfterPlayInComplete();
+
+        internal void InternalPlayOut()
+        {
+            PlayOut();
+        }
+        
+        protected abstract void PlayOut();
+
+        protected virtual void PlayOutComplete()
+        {
+            OnPlayOutComplete?.Invoke();
+        }
+
+        public void Clear()
+        {
+            OnClear?.Invoke();
+        }
+        
+
+    }
+}
+
---
+++ Scripts/Runtime/Content/AbstractContentGroup.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Anvil.CSharp.Core;
+using Anvil.CSharp.DelayedExecution;
+
+namespace Anvil.CSharp.Content
+{
+    public abstract class AbstractContentGroup : AbstractAnvilDisposable
+    {
+        public event Action<AbstractContentController> OnLoadStart;
+        public event Action<AbstractContentController> OnLoadComplete;
+        public event Action<AbstractContentController> OnPlayInStart;
+        public event Action<AbstractContentController> OnPlayInComplete;
+        public event Action<AbstractContentController> OnPlayOutStart;
+        public event Action<AbstractContentController> OnPlayOutComplete;
+
+
+        public readonly string ID;
+        public Transform ContentGroupRoot { get; private set; }
+        
+        public AbstractContentManager<,,> ContentManager { get; private set; }
+        
+        public AbstractContentController ActiveContentController { get; private set; }
+        private AbstractContentController m_PendingContentController;
+
+        private UpdateHandle m_UpdateHandle;
+        
+        //TODO: Snippet about the gameObjectRoot. To be cleaned up when docs pass happens on this class.
+        // /// <summary>
+        // /// A custom user supplied <see cref="GameObject"/> <see cref="Transform"/> to parent this
+        // /// <see cref="ContentGroup"/> to. If left null (the default), the <see cref="ContentGroup"/> will be parented
+        // /// to the <see cref="ContentManager"/>'s ContentRoot.
+        // /// </summary>
+
+        public AbstractContentGroup()
+        {
+            ContentManager = contentManager;
+            ID = id;
+
+            m_UpdateHandle = UpdateHandle.Create<UnityUpdateSource>();
+
+            InitGameObject(localPosition, gameObjectRoot);
+        }
+
+        protected override void DisposeSelf()
+        {
+            if (m_UpdateHandle != null)
+            {
+                m_UpdateHandle.Dispose();
+                m_UpdateHandle = null;
+            }
+            base.DisposeSelf();
+        }
+
+        private void InitGameObject(Vector3 localPosition, Transform gameObjectRoot)
+        {
+            GameObject groupRootGO = new GameObject($"[CL - {ID}]");
+            ContentGroupRoot = groupRootGO.transform;
+            Transform parent = gameObjectRoot == null
+                ? ContentManager.ContentRoot
+                : gameObjectRoot;
+            ContentGroupRoot.SetParent(parent);
+            ContentGroupRoot.localPosition = localPosition;
+            ContentGroupRoot.localRotation = Quaternion.identity;
+            ContentGroupRoot.localScale = Vector3.one;
+        }
+
+        public void Show(AbstractContentController contentController)
+        {
+            //TODO: Validate the passed in controller to ensure we avoid weird cases such as:
+            // - Showing the same instance that is already showing or about to be shown
+            // - Might have a pending controller in the process of loading
+            
+            m_PendingContentController = contentController;
+
+            //If there's an Active Controller currently being shown, we need to clear it.
+            if (ActiveContentController != null)
+            {
+                OnPlayOutStart?.Invoke(ActiveContentController);
+                ActiveContentController.InternalPlayOut();
+            }
+            else
+            {
+                //Otherwise we can just show the pending controller
+                ShowPendingContentController();
+            }
+        }
+
+        public void Clear()
+        {
+            Show(null);
+        }
+
+        private void ShowPendingContentController()
+        {
+            if (m_PendingContentController == null)
+            {
+                return;
+            }
+            //We can't show the pending controller right away because we may not have the necessary assets loaded. 
+            //So we need to construct a Sequential Command and populate with the required commands to load the assets needed. 
+            
+            
+            ActiveContentController = m_PendingContentController;
+            m_PendingContentController = null;
+            ActiveContentController.ContentGroup = this;
+
+            OnLoadStart?.Invoke(ActiveContentController);
+            ActiveContentController.OnLoadComplete += HandleOnLoadComplete;
+            ActiveContentController.Load();
+        }
+
+        private void HandleOnLoadComplete()
+        {
+            ActiveContentController.OnLoadComplete -= HandleOnLoadComplete;
+            OnLoadComplete?.Invoke(ActiveContentController);
+            
+            ActiveContentController.InternalInitAfterLoadComplete();
+
+            AbstractContent content = ActiveContentController.Content;
+            Transform transform = content.transform;
+            transform.SetParent(ContentGroupRoot);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+
+            AttachLifeCycleListeners(ActiveContentController);
+            OnPlayInStart?.Invoke(ActiveContentController);
+            ActiveContentController.InternalPlayIn();
+        }
+
+        private void AttachLifeCycleListeners(AbstractContentController contentController)
+        {
+            contentController.OnPlayInComplete += HandleOnPlayInComplete;
+            contentController.OnPlayOutComplete += HandleOnPlayOutComplete;
+            contentController.OnClear += HandleOnClear;
+        }
+        
+        private void RemoveLifeCycleListeners(AbstractContentController contentController)
+        {
+            contentController.OnPlayInComplete -= HandleOnPlayInComplete;
+            contentController.OnPlayOutComplete -= HandleOnPlayOutComplete;
+            contentController.OnClear -= HandleOnClear;
+        }
+
+        private void HandleOnPlayInComplete()
+        {
+            ActiveContentController.OnPlayInComplete -= HandleOnPlayInComplete;
+            OnPlayInComplete?.Invoke(ActiveContentController);
+
+            ActiveContentController.InternalInitAfterPlayInComplete();
+        }
+
+        private void HandleOnPlayOutComplete()
+        {
+            if (ActiveContentController != null)
+            {
+                OnPlayOutComplete?.Invoke(ActiveContentController);
+                RemoveLifeCycleListeners(ActiveContentController);
+                ActiveContentController.Dispose();
+                ActiveContentController = null;
+            }
+
+            ShowPendingContentController();
+        }
+
+        private void HandleOnClear()
+        {
+            Clear();
+        }
+
+        
+        
+        
+    }
+}
+
