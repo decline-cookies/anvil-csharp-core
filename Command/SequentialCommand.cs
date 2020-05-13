47726d81517ef3d765740da9591f05deabc70685
Jon Keon <jon.keon@gmail.com>
2020-05-12 20:59:22 -0400

    Implemented the ability to handle JIT creation of Commands.

--- Command/SequentialCommand.cs
+++ Command/SequentialCommand.cs
@@ -9,7 +9,29 @@ namespace Anvil.CSharp.Command
     /// </summary>
     public class SequentialCommand : AbstractCollectionCommand
     {
+        /// <summary>
+        /// Creates an instance of a <see cref="IJITInitCommand"/> that will instantiate a command of type
+        /// <see cref="TToType"/> right before execution and provide reference to the previously completed command
+        /// of type <see cref="TFromType"/>. The <see cref="jitInitFunction"/> is provided by the user and provides the
+        /// "glue" to create the new command with data/results from the previous command.
+        /// This is useful when needing to use the <see cref="SequentialCommand"/> constructors or
+        /// <see cref="AddChildren"/> and <see cref="InsertChildren"/> functions.
+        /// </summary>
+        /// <param name="jitInitFunction"><inheritdoc cref="JITInitCommandFunction{TFromType,TToType}"/></param>
+        /// <typeparam name="TFromType">The type of the previously completed command.</typeparam>
+        /// <typeparam name="TToType">The type of the new command to initialize.</typeparam>
+        /// <returns>An instance of <see cref="IJITInitCommand"/></returns>
+        public static IJITInitCommand CreateJITInitCommand<TFromType, TToType>(JITInitCommandFunction<TFromType, TToType> jitInitFunction)
+            where TFromType : AbstractCommand
+            where TToType : AbstractCommand
+        {
+            JITInitCommand<TFromType, TToType> jitInitCommand = new JITInitCommand<TFromType, TToType>(jitInitFunction);
+            return jitInitCommand;
+        }
+
         private int m_ChildCommandIndex;
+        private AbstractCommand m_PreviousChildCommand;
+
         /// <summary>
         /// Constructs a <see cref="SequentialCommand"/> using params for <see cref="AbstractCommand"/>.
         /// </summary>
@@ -17,7 +39,7 @@ namespace Anvil.CSharp.Command
         public SequentialCommand(params AbstractCommand[] childCommands) : base (childCommands)
         {
         }
-        
+
         /// <summary>
         /// Constructs a <see cref="SequentialCommand"/> using an <see cref="IEnumerable{AbstractCommand}"/>.
         /// </summary>
@@ -28,11 +50,49 @@ namespace Anvil.CSharp.Command
 
         protected override void DisposeSelf()
         {
+            m_PreviousChildCommand = null;
             m_ChildCommandIndex = 0;
-            
+
             base.DisposeSelf();
         }
-        
+
+        /// <summary>
+        /// Adds a child command to be executed in the collection. This child command will be instantiated just
+        /// before execution.
+        /// <see cref="CreateJITInitCommand{TFromType,TToType}"/>
+        /// </summary>
+        /// <param name="jitInitFunction"><inheritdoc cref="JITInitCommandFunction{TFromType,TToType}"/></param>
+        /// <typeparam name="TFromType">The type of the previously completed command.</typeparam>
+        /// <typeparam name="TToType">The type of the new command to initialize.</typeparam>
+        /// <returns>The <see cref="AbstractCollectionCommand"/> the child was added to. Useful for method chaining.</returns>
+        public AbstractCollectionCommand AddChild<TFromType, TToType>(JITInitCommandFunction<TFromType, TToType> jitInitFunction)
+            where TFromType : AbstractCommand
+            where TToType : AbstractCommand
+        {
+            IJITInitCommand jitInitCommand = CreateJITInitCommand<TFromType, TToType>(jitInitFunction);
+
+            return AddChild(jitInitCommand as AbstractCommand);;
+        }
+
+        /// <summary>
+        /// Inserts a child command to be executed in the collection. This child command will be instantiated just
+        /// before execution.
+        /// <see cref="CreateJITInitCommand{TFromType,TToType}"/>
+        /// </summary>
+        /// <param name="index">The index for when the command should be executed.</param>
+        /// <param name="jitInitFunction"><inheritdoc cref="JITInitCommandFunction{TFromType,TToType}"/></param>
+        /// <typeparam name="TFromType">The type of the previously completed command.</typeparam>
+        /// <typeparam name="TToType">The type of the new command to initialize.</typeparam>
+        /// <returns>The <see cref="AbstractCollectionCommand"/> the child was inserted into. Useful for method chaining.</returns>
+        public AbstractCollectionCommand InsertChild<TFromType, TToType>(int index, JITInitCommandFunction<TFromType, TToType> jitInitFunction)
+            where TFromType : AbstractCommand
+            where TToType : AbstractCommand
+        {
+            IJITInitCommand jitInitCommand = CreateJITInitCommand<TFromType, TToType>(jitInitFunction);
+
+            return InsertChild(index, jitInitCommand as AbstractCommand);
+        }
+
         /// <summary>
         /// Inserts a child command to be executed in the collection.
         /// </summary>
@@ -46,7 +106,7 @@ namespace Anvil.CSharp.Command
             {
                 throw new Exception($"Tried to insert child command {childCommand} to {this} but State was {State} instead of Initialized!");
             }
-            
+
             m_ChildCommands.Insert(index, childCommand);
 
             return this;
@@ -74,7 +134,7 @@ namespace Anvil.CSharp.Command
             m_ChildCommandIndex = 0;
             ExecuteNextChildCommandInSequence();
         }
-        
+
         private void ExecuteNextChildCommandInSequence()
         {
             if (m_ChildCommandIndex >= m_ChildCommands.Count)
@@ -82,16 +142,28 @@ namespace Anvil.CSharp.Command
                 CompleteCommand();
                 return;
             }
-            
-            AbstractCommand childCommand = m_ChildCommands[m_ChildCommandIndex];
-            childCommand.OnComplete += HandleChildCommandOnComplete;
-            childCommand.Execute();
+
+            AbstractCommand nextChildCommand = m_ChildCommands[m_ChildCommandIndex];
+
+            //Handles actually creating the command needed if this is a command that should be created just in time.
+            if (nextChildCommand is IJITInitCommand jitInitCommand)
+            {
+                nextChildCommand = jitInitCommand.JITInit(m_PreviousChildCommand);
+                (jitInitCommand as AbstractCommand).Dispose();
+            }
+            m_PreviousChildCommand = null;
+
+            nextChildCommand.OnComplete += HandleChildCommandOnComplete;
+            nextChildCommand.Execute();
         }
 
         private void HandleChildCommandOnComplete(AbstractCommand command)
         {
             command.OnComplete -= HandleChildCommandOnComplete;
             m_ChildCommandIndex++;
+
+            m_PreviousChildCommand = command;
+
             ExecuteNextChildCommandInSequence();
         }
     }
