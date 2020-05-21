c369e711a752ee5592f4cd740c0e0ae0d7deba76
Jon Keon <jon.keon@gmail.com>
2020-05-21 12:59:33 -0400

    PR Changes

--- Command/BufferCommand.cs
+++ Command/BufferCommand.cs
@@ -5,26 +5,30 @@ namespace Anvil.CSharp.Command
 {
     /// <summary>
     /// The Buffer Command is an <see cref="ICommand"/> that is not intended to ever complete.
-    /// Instead it "stays open" and allows for more child <see cref="ICommand"/>s to be added to it
-    /// along with a priority. Commands will be executed one after the other in priority order until
+    /// Instead it "stays open" and allows for more child <see cref="ICommand"/>s to be added to it.
+    /// Commands will be executed one after the other in order until
     /// there are none left and then it will wait until more commands are added.
     /// </summary>
     public class BufferCommand : AbstractCommand<BufferCommand>
     {
-        private readonly List<ICommand> m_ChildCommands = new List<ICommand>();
-        private readonly List<float> m_ChildPriorities = new List<float>();
+        /// <summary>
+        /// Dispatches when the buffer of commands is empty. The <see cref="BufferCommand"/> will be in an idle state
+        /// and able to accept more commands at any time.
+        /// </summary>
+        public event Action<BufferCommand> OnBufferEmpty;
 
+        private readonly Queue<ICommand> m_ChildCommands = new Queue<ICommand>();
         private ICommand m_ActiveCommand;
 
-
         /// <summary>
-        /// <see cref="BufferCommand"/> will throw an <see cref="InvalidOperationException"/> if the
+        /// <see cref="BufferCommand"/> will throw an <see cref="NotSupportedException"/> if the
         /// OnComplete event is subscribed to.
         /// </summary>
+        [Obsolete("Buffer Commands are designed to never complete!", true)]
         public new event Action<BufferCommand> OnComplete
         {
-            add => throw new InvalidOperationException($"Buffer Commands are designed to never complete!");
-            remove => throw new InvalidOperationException($"Buffer Commands are designed to never complete!");
+            add => throw new NotSupportedException($"Buffer Commands are designed to never complete!");
+            remove => throw new NotSupportedException($"Buffer Commands are designed to never complete!");
         }
 
         /// <summary>
@@ -36,40 +40,19 @@ namespace Anvil.CSharp.Command
         }
 
         /// <summary>
-        /// Creates a new instance of a <see cref="BufferCommand"/> that takes in
-        /// <see cref="ICommand"/>/<see cref="float"/> tuples to initially populate.
-        /// </summary>
-        /// <param name="children">A set of <see cref="ICommand"/>/<see cref="float"/> tuple to populate with.</param>
-        public BufferCommand(params (ICommand Command, float Priority)[] children):this((IEnumerable<(ICommand Command, float Priority)>)children)
-        {
-        }
-
-        /// <summary>
         /// Creates a new instance of a <see cref="BufferCommand"/> that takes in a set of
-        /// <see cref="ICommand"/>s. Assumes a priority of 0.0f for each Command.
+        /// <see cref="ICommand"/>s.
         /// </summary>
-        /// <param name="childCommands">A set of <see cref="ICommand"/>s to populate with. They all have the same
-        /// priority of 0.0f.</param>
+        /// <param name="childCommands">A set of <see cref="ICommand"/>s to populate with.</param>
         public BufferCommand(params ICommand[] childCommands):this((IEnumerable<ICommand>)childCommands)
         {
         }
 
         /// <summary>
-        /// Creates a new instance of a <see cref="BufferCommand"/> that takes in a
-        /// <see cref="Enumerable{(ICommand, float)}"/> tuples to initially populate.
-        /// </summary>
-        /// <param name="children">An <see cref="IEnumerable{(ICommand, float)}"/> tuples to populate with.</param>
-        public BufferCommand(IEnumerable<(ICommand Command, float Priority)> children)
-        {
-            AddChildren(children);
-        }
-
-        /// <summary>
         /// Creates a new instance of a <see cref="BufferCommand"/> that takes in a <see cref="IEnumerable{ICommand}"/>
-        /// to populate with. Assumes a priority of 0.0f for each Command.
+        /// to populate with.
         /// </summary>
-        /// <param name="childCommands">A set of <see cref="IEnumerable{ICommand}"/>s to populate with. They all have
-        /// the same priority of 0.0f.</param>
+        /// <param name="childCommands">A set of <see cref="IEnumerable{ICommand}"/>s to populate with.</param>
         public BufferCommand(IEnumerable<ICommand> childCommands)
         {
             AddChildren(childCommands);
@@ -77,13 +60,10 @@ namespace Anvil.CSharp.Command
 
         protected override void DisposeSelf()
         {
-            foreach (ICommand command in m_ChildCommands)
-            {
-                command.Dispose();
-            }
-            m_ChildCommands.Clear();
-            m_ChildPriorities.Clear();
+            Clear();
+            OnBufferEmpty = null;
 
+            m_ActiveCommand?.Dispose();
             m_ActiveCommand = null;
 
             base.DisposeSelf();
@@ -93,44 +73,10 @@ namespace Anvil.CSharp.Command
         /// Adds a <see cref="ICommand"/> to be executed as part of the <see cref="BufferCommand"/>.
         /// </summary>
         /// <param name="childCommand">The <see cref="ICommand"/> to execute.</param>
-        /// <param name="priority">The priority of the <see cref="ICommand"/>.
-        /// This is a <see cref="float"/> value where the higher the number, the higher the priority. Commands
-        /// with the same priority will execute in the order they were added.</param>
-        /// <param name="shouldRemoveFollowingCommands">Optional flag for whether all commands in the buffer AFTER the
-        /// passed in childCommand should be removed and disposed. Useful for when you need to interrupt an
-        /// existing queue of commands. Note that all commands removed will be disposed.</param>
         /// <returns>A reference to this <see cref="BufferCommand"/>. Useful for method chaining.</returns>
-        public BufferCommand AddChild(ICommand childCommand, float priority = 0.0f, bool shouldRemoveFollowingCommands = false)
+        public BufferCommand AddChild(ICommand childCommand)
         {
-            //Assume we're adding to the end of the queue
-            int insertIndex = m_ChildCommands.Count;
-
-            //Iterate through to find where we should insert based on our priority.
-            //TODO: Might be able to optimize this to find the insertion faster? Wait until it matters. Likely only a few in the queue at a time.
-            for (int i = 0; i < m_ChildCommands.Count; ++i)
-            {
-                float currentPriority = m_ChildPriorities[i];
-                if (priority > currentPriority)
-                {
-                    insertIndex = i;
-                    break;
-                }
-            }
-
-            //Ensure Commands and Priority queues match.
-            m_ChildCommands.Insert(insertIndex, childCommand);
-            m_ChildPriorities.Insert(insertIndex, priority);
-
-            if (shouldRemoveFollowingCommands)
-            {
-                //Normally could do a RemoveRange, but we want to ensure we Dispose
-                for (int i = m_ChildCommands.Count - 1; i > insertIndex; --i)
-                {
-                    ICommand command = m_ChildCommands[i];
-                    command.Dispose();
-                    m_ChildCommands.RemoveAt(i);
-                }
-            }
+            m_ChildCommands.Enqueue(childCommand);
 
             //If the Buffer has been started and we're not currently executing anything, we should kick off the next command
             if (State == CommandState.Executing && m_ActiveCommand == null)
@@ -142,23 +88,7 @@ namespace Anvil.CSharp.Command
         }
 
         /// <summary>
-        /// Adds a <see cref="IEnumerable{(ICommand, float}"/> set of tuples to the Buffer Command to be executed.
-        /// </summary>
-        /// <param name="children">The <see cref="IEnumerable{(ICommand, float}"/> set of tuples to add.</param>
-        /// <returns>A reference to this <see cref="BufferCommand"/>. Useful for method chaining.</returns>
-        public BufferCommand AddChildren(IEnumerable<(ICommand Command, float Priority)> children)
-        {
-            foreach ((ICommand command, float priority) in children)
-            {
-                AddChild(command, priority);
-            }
-
-            return this;
-        }
-
-        /// <summary>
-        /// Adds a <see cref="IEnumerable{(ICommand}"/> set of tuples to the Buffer Command to be executed.
-        /// All priorities for the Commands will be 0.0f
+        /// Adds a <see cref="IEnumerable{(ICommand}"/> to the Buffer Command to be executed.
         /// </summary>
         /// <param name="children">The <see cref="IEnumerable{(ICommand}"/> to add.</param>
         /// <returns>A reference to this <see cref="BufferCommand"/>. Useful for method chaining.</returns>
@@ -172,6 +102,36 @@ namespace Anvil.CSharp.Command
             return this;
         }
 
+        /// <summary>
+        /// Clears all queued commands in the <see cref="BufferCommand"/> and disposes them.
+        /// Will dispatch <see cref="BufferCommand.OnBufferEmpty"/> so long as the <see cref="BufferCommand"/> is
+        /// not Disposing or Disposed.
+        ///
+        /// This function will do no work and will not dispatch <see cref="BufferCommand.OnBufferEmpty"/> if the buffer
+        /// is already empty.
+        ///
+        /// The active command currently being executed will not be affected by this function.
+        /// </summary>
+        public void Clear()
+        {
+            //Early return to prevent duplicate firing of OnBufferEmpty
+            if (m_ChildCommands.Count == 0)
+            {
+                return;
+            }
+
+            foreach (ICommand childCommand in m_ChildCommands)
+            {
+                childCommand.Dispose();
+            }
+            m_ChildCommands.Clear();
+
+            if (!IsDisposing && !IsDisposed)
+            {
+                OnBufferEmpty?.Invoke(this);
+            }
+        }
+
         protected override void ExecuteCommand()
         {
             ExecuteNextChildCommandInBuffer();
@@ -179,19 +139,24 @@ namespace Anvil.CSharp.Command
 
         private void ExecuteNextChildCommandInBuffer()
         {
+            //Early return if the buffer is empty and we want to idle.
             if (m_ChildCommands.Count == 0)
             {
                 return;
             }
 
-            ICommand childCommand = m_ChildCommands[0];
-
-            m_ChildCommands.RemoveAt(0);
-            m_ChildPriorities.RemoveAt(0);
+            //Kick off the next command in the buffer
+            ICommand childCommand = m_ChildCommands.Dequeue();
 
             m_ActiveCommand = childCommand;
             m_ActiveCommand.OnComplete += ChildCommand_OnComplete;
             m_ActiveCommand.Execute();
+
+            //Second check to see if that was the last command in the buffer to alert that the buffer is empty
+            if (m_ChildCommands.Count == 0)
+            {
+                OnBufferEmpty?.Invoke(this);
+            }
         }
 
         private void ChildCommand_OnComplete(ICommand childCommand)
