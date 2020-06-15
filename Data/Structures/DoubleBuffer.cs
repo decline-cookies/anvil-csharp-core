cec62f4453b2bb98ed9e0cf64afb300033327942
Jon Keon <jon.keon@gmail.com>
2020-06-15 13:31:37 -0400

    Adding Double Buffer structure

---
+++ Data/Structures/DoubleBuffer.cs
@@ -0,0 +1,20 @@
+﻿using Anvil.CSharp.Core;
+
+namespace Anvil.CSharp.Data
+{
+    public class DoubleBuffer<T> : AbstractAnvilDisposable
+        where T:class, new()
+    {
+        private readonly T[] m_Buffers = {new T(), new T()};
+        private int m_CurrentIndex;
+
+        public T Current => m_Buffers[m_CurrentIndex];
+        public T Previous => m_Buffers[1 - m_CurrentIndex];
+
+        public void SwapBuffer()
+        {
+            m_CurrentIndex = 1 - m_CurrentIndex;
+        }
+    }
+}
+
