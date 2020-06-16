4d19aedc02712175679d4437c0b9c1992d7e0079
Jon Keon <jon.keon@gmail.com>
2020-06-16 14:11:40 -0400

    Docs pass

--- Data/Structures/DoubleBuffer.cs
+++ Data/Structures/DoubleBuffer.cs
@@ -1,16 +1,31 @@
-﻿using Anvil.CSharp.Core;
-
-namespace Anvil.CSharp.Data
+﻿namespace Anvil.CSharp.Data
 {
-    public class DoubleBuffer<T> : AbstractAnvilDisposable
-        where T:class, new()
+    /// <summary>
+    /// A data structure to hold two of the same objects for easy comparision over time.
+    /// </summary>
+    /// <remarks>
+    /// Commonly
+    /// </remarks>
+    /// <typeparam name="T">The type of object to compare.</typeparam>
+    public class DoubleBuffer<T>
+        where T:new()
     {
         private readonly T[] m_Buffers = {new T(), new T()};
         private int m_CurrentIndex;
 
+        /// <summary>
+        /// The Current buffer to use.
+        /// </summary>
         public T Current => m_Buffers[m_CurrentIndex];
+
+        /// <summary>
+        /// The Previous buffer that was used.
+        /// </summary>
         public T Previous => m_Buffers[1 - m_CurrentIndex];
 
+        /// <summary>
+        /// Swaps the <see cref="Current"/> and <see cref="Previous"/> buffer.
+        /// </summary>
         public void SwapBuffer()
         {
             m_CurrentIndex = 1 - m_CurrentIndex;
