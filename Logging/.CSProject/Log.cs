a484614431b30ccf516d98b5ce0d1f6f53451c3d
Tyler Vezina <tylerjvezina@gmail.com>
2020-10-21 18:43:46 -0400

    Log functions accept any `object` instead of only `string`

--- Logging/.CSProject/Log.cs
+++ Logging/.CSProject/Log.cs
@@ -47,13 +47,13 @@ namespace Anvil.CSharp.Logging
         public static bool AddHandler(ILogHandler handler) => (handler != null && s_AdditionalHandlerList.Add(handler));
         public static bool RemoveHandler(ILogHandler handler) => s_AdditionalHandlerList.Remove(handler);
 
-        public static void Debug(string message) => DispatchLog(LogLevel.Debug, message);
+        public static void Debug(object message) => DispatchLog(LogLevel.Debug, $"{message}");
         public static void DebugFormat(string format, params object[] args) => Debug(string.Format(format, args));
 
-        public static void Warning(string message) => DispatchLog(LogLevel.Warning, message);
+        public static void Warning(object message) => DispatchLog(LogLevel.Warning, $"{message}");
         public static void WarningFormat(string format, params object[] args) => Warning(string.Format(format, args));
 
-        public static void Error(string message) => DispatchLog(LogLevel.Error, message);
+        public static void Error(object message) => DispatchLog(LogLevel.Error, $"{message}");
         public static void ErrorFormat(string format, params object[] args) => Error(string.Format(format, args));
 
         private static void DispatchLog(LogLevel level, string message)
