4fae0a424c696c4d00a615df068b2386a6fb6817
Jon Keon <jon.keon@gmail.com>
2021-03-11 14:49:28 -0500

    IO functions

---
+++ IO/LoadFromDiskCommand.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using Anvil.CSharp.Command;
+
+namespace Anvil.IO
+{
+    public class LoadFromDiskCommand : AbstractCommand<LoadFromDiskCommand>, ISuccessfulCommand
+    {
+        public bool WasSuccessful
+        {
+            get;
+            private set;
+        }
+
+        public AggregateException Exception
+        {
+            get;
+            private set;
+        }
+
+        public byte[] Data
+        {
+            get;
+            private set;
+        }
+        
+        private readonly string m_Path;
+
+        private FileStream m_FileStream;
+
+        public LoadFromDiskCommand(string path)
+        {
+            m_Path = path;
+        }
+
+        protected override void DisposeSelf()
+        {
+            m_FileStream?.Dispose();
+            
+            base.DisposeSelf();
+        }
+
+        protected override void ExecuteCommand()
+        {
+            ExecuteAsync();
+        }
+
+        private async void ExecuteAsync()
+        {
+            if (!File.Exists(m_Path))
+            {
+                WasSuccessful = false;
+                Exception = new AggregateException(new FileNotFoundException($"File at path {m_Path} doesn't exist!"));
+                CompleteCommand();
+            }
+
+            m_FileStream = File.Open(m_Path, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            int totalBytes = (int)m_FileStream.Length;
+            
+            Data = new byte[totalBytes];
+
+            try
+            {
+                await m_FileStream.ReadAsync(Data, 0, totalBytes);
+            }
+            catch (Exception e)
+            {
+                WasSuccessful = false;
+                Exception = new AggregateException(e);
+                CompleteCommand();
+            }
+
+            WasSuccessful = true;
+            CompleteCommand();
+        }
+    }
+}
+
---
+++ IO/WriteToDiskCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Anvil.CSharp.Command;
+
+namespace Anvil.IO
+{
+    public class WriteToDiskCommand : AbstractCommand<WriteToDiskCommand>
+    {
+        public bool WasSuccessful
+        {
+            get;
+            private set;
+        }
+
+        public Exception Exception
+        {
+            get;
+            private set;
+        }
+
+        private readonly string m_Path;
+        private readonly byte[] m_Data;
+        private readonly CancellationTokenSource m_CancellationTokenSource;
+
+        private FileStream m_FileStream;
+
+        public WriteToDiskCommand(string path, byte[] data)
+        {
+            m_Path = path;
+            m_Data = data;
+            m_CancellationTokenSource = new CancellationTokenSource();
+        }
+
+        protected override void DisposeSelf()
+        {
+            m_CancellationTokenSource.Dispose();
+            m_FileStream?.Dispose();
+            
+            base.DisposeSelf();
+        }
+
+        protected override void ExecuteCommand()
+        {
+            ExecuteAsync();
+        }
+
+        private async void ExecuteAsync()
+        {
+            string directoryPath = Path.GetDirectoryName(m_Path);
+            if (!Directory.Exists(directoryPath))
+            {
+                //TODO: Handle null case
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            m_FileStream = File.Open(m_Path, FileMode.Create, FileAccess.Write, FileShare.Write);
+
+            int totalBytes = m_Data.Length;
+
+            try
+            {
+                await m_FileStream.WriteAsync(m_Data, 0, totalBytes, m_CancellationTokenSource.Token);
+            }
+            catch (Exception e)
+            {
+                WasSuccessful = false;
+                Exception = e;
+                CompleteCommand();
+            }
+
+            WasSuccessful = true;
+            CompleteCommand();
+        }
+    }
+}
+
