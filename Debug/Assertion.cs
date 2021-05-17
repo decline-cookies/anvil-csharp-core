b4d8bf196900304ff35bd171252f2201a71851a3
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2021-05-17 11:31:11 -0400

    Assertion now provides complext condition checks.
    The class no longer asserts. This allows more flexible usage

--- Debug/Assertion.cs
+++ Debug/Assertion.cs
@@ -3,13 +3,23 @@ using System.Runtime.CompilerServices;
 
 namespace Anvil.CSharp.Debugging
 {
+    /// <summary>
+    /// A collection of convenient checks to make it easier/clearner for developers to assert
+    /// complex conditions.
+    /// </summary>
     public static class Assertion
     {
+        /// <summary>
+        /// Returns true if the executing method is being called by a method with the provided name.
+        /// </summary>
+        /// <param name="callerName">The method name of the expected caller.</param>
+        /// <example>Debug.Assert(IsCaller(nameof(MyExpectedCallingMethod)))</example>
         [MethodImpl(MethodImplOptions.NoInlining)]
-        [Conditional("DEBUG")]
-        public static void IsCaller(string callerName, string message)
+        public static bool IsCaller(string callerName)
         {
-            Debug.Assert(new StackFrame(2).GetMethod().Name == callerName, message);
+            // Look at the frame two levels up because that's the one calling the method
+            // that calls this method.
+            return new StackFrame(2).GetMethod().Name == callerName;
         }
     }
 }
