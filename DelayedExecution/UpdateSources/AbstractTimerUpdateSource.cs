e5d2f1a45102de43fab906fc6390369c5fafff5f
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2021-08-24 12:24:44 -0400

    AbstractTimerUpdateSource - Cleanup

--- DelayedExecution/UpdateSources/AbstractTimerUpdateSource.cs
+++ DelayedExecution/UpdateSources/AbstractTimerUpdateSource.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Anvil.CSharp.Logging;
+using System;
 using System.Threading;
 
 namespace Anvil.CSharp.DelayedExecution
@@ -6,37 +7,48 @@ namespace Anvil.CSharp.DelayedExecution
     //TODO: Document
     //TODO: Explore code gen to avoid having the developer subclass. (Related: AbstractOffThreadUpdateSource)
     //Subclass and create a constructorless implementation that locks in the interval and thread marshalling
+    // WARNING: Very short intervals may not work as expected. see https://docs.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-5.0#remarks
     public abstract class AbstractTimerUpdateSource : AbstractUpdateSource
     {
-        private readonly Timer m_Timer;
+        private Timer m_Timer;
 
         protected AbstractTimerUpdateSource(float updateInterval)
         {
-            int updateIntervalMs = (int)SecondsToMs(updateInterval);
-
-            m_Timer = new Timer(
-                (state) => DispatchOnUpdateEvent(), 
-                null, 
-                updateIntervalMs, 
-                updateIntervalMs);
+            InitTimer(updateInterval, (state) => DispatchOnUpdateEvent());
         }
 
         protected AbstractTimerUpdateSource(float updateInterval, Action<Action> onThreadMarshall)
         {
+            InitTimer(updateInterval, (state) => onThreadMarshall(DispatchOnUpdateEvent));
+        }
+
+        private void InitTimer(float updateInterval, TimerCallback callback)
+        {
             int updateIntervalMs = (int)SecondsToMs(updateInterval);
+            WarnIfInvalidInterval(updateIntervalMs);
 
             m_Timer = new Timer(
-                (state) => onThreadMarshall(DispatchOnUpdateEvent), 
-                null, 
-                updateIntervalMs, 
+                callback,
+                null,
+                updateIntervalMs,
                 updateIntervalMs);
         }
 
         protected override void DisposeSelf()
         {
-            m_Timer.Dispose();
+            m_Timer?.Dispose();
 
             base.DisposeSelf();
+        }
+
+        private static void WarnIfInvalidInterval(int interval)
+        {
+            // Update interval less than 15ms will not work as expected.
+            // https://docs.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-5.0#remarks
+            if (interval < 15)
+            {
+                Log.Warning("Intervals less than 15ms will not work reliably.\n See https://docs.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-5.0#remarks");
+            }
         }
 
         private static float SecondsToMs(float seconds) => MathF.Round(seconds * 1000);
