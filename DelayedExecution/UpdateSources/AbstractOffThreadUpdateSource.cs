e659ea054ebbe4b1fa0b270d4a9d5b90e6d2b01c
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2021-08-24 11:25:57 -0400

    Implement AbstractTimerUpdateSource and AbstractOffThreadUpdateSource

---
+++ DelayedExecution/UpdateSources/AbstractOffThreadUpdateSource.cs
@@ -0,0 +1,67 @@
+﻿using Anvil.CSharp.DelayedExecution;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Anvil.CSharp.DelayedExecution
+{
+    //TODO: Keep? See AbstractTimerUpdateSource
+    public class AbstractOffThreadUpdateSource : AbstractUpdateSource
+    {
+        private readonly CancellationTokenSource m_CancelationTokenSource;
+        private readonly int m_UpdateIntervalMs;
+        private readonly Action m_OnIntervalAction;
+
+        public AbstractOffThreadUpdateSource(float updateInterval)
+        {
+            m_UpdateIntervalMs = (int)SecondsToMs(updateInterval);
+            m_OnIntervalAction = DispatchOnUpdateEvent;
+
+            m_CancelationTokenSource = new CancellationTokenSource();
+            Start();
+        }
+
+        public AbstractOffThreadUpdateSource(float updateInterval, Action<Action> onThreadMarshall)
+        {
+            m_UpdateIntervalMs = (int)SecondsToMs(updateInterval);
+            m_OnIntervalAction = () => onThreadMarshall(DispatchOnUpdateEvent);
+            
+            m_CancelationTokenSource = new CancellationTokenSource();
+            Start();
+        }
+
+        protected override void DisposeSelf()
+        {
+            Stop();
+            m_CancelationTokenSource.Dispose();
+
+            base.DisposeSelf();
+        }
+
+        private void Start()
+        {
+            Task result = Task.Run(
+                () => RunUpdateLoop(m_UpdateIntervalMs, m_OnIntervalAction), 
+                m_CancelationTokenSource.Token);
+        }
+
+        private void Stop()
+        {
+            m_CancelationTokenSource.Cancel();
+        }
+
+        private static float SecondsToMs(float seconds) => MathF.Round(seconds * 1000);
+
+        private static async Task RunUpdateLoop(int updateInterval, Action onInterval)
+        {
+            while (true)
+            {
+                await Task.Delay(updateInterval);
+                onInterval();
+            }
+        }
+
+
+    }
+}
---
+++ DelayedExecution/UpdateSources/AbstractTimerUpdateSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Anvil.CSharp.DelayedExecution
+{
+    //TODO: Document
+    //TODO: Explore code gen to avoid having the developer subclass. (Related: AbstractOffThreadUpdateSource)
+    //Subclass and create a constructorless implementation that locks in the interval and thread marshalling
+    public abstract class AbstractTimerUpdateSource : AbstractUpdateSource
+    {
+        private readonly Timer m_Timer;
+
+        protected AbstractTimerUpdateSource(float updateInterval)
+        {
+            int updateIntervalMs = (int)SecondsToMs(updateInterval);
+
+            m_Timer = new Timer(
+                (state) => DispatchOnUpdateEvent(), 
+                null, 
+                updateIntervalMs, 
+                updateIntervalMs);
+        }
+
+        protected AbstractTimerUpdateSource(float updateInterval, Action<Action> onThreadMarshall)
+        {
+            int updateIntervalMs = (int)SecondsToMs(updateInterval);
+
+            m_Timer = new Timer(
+                (state) => onThreadMarshall(DispatchOnUpdateEvent), 
+                null, 
+                updateIntervalMs, 
+                updateIntervalMs);
+        }
+
+        protected override void DisposeSelf()
+        {
+            m_Timer.Dispose();
+
+            base.DisposeSelf();
+        }
+
+        private static float SecondsToMs(float seconds) => MathF.Round(seconds * 1000);
+    }
+}
