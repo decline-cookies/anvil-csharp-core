bc5a8913554703f61e5fe6f8b4d8cbfe91058873
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2021-08-25 14:45:49 -0400

    PR #56 Feedback
    - Document UpdateSources
    - Clean up method naming
    
    AbstractThreadSleepUpdateSource
    - Clean up constructors
    - Fix cancellation/disposal bug
    - Fix spelling mistakes

--- DelayedExecution/UpdateSources/AbstractTimerUpdateSource.cs
+++ DelayedExecution/UpdateSources/AbstractTimerUpdateSource.cs
@@ -4,10 +4,16 @@ using System.Threading;
 
 namespace Anvil.CSharp.DelayedExecution
 {
-    //TODO: Document
     //TODO: Explore code gen to avoid having the developer subclass. (Related: AbstractOffThreadUpdateSource)
     //Subclass and create a constructorless implementation that locks in the interval and thread marshalling
-    // WARNING: Very short intervals may not work as expected. see https://docs.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-5.0#remarks
+    
+    /// <summary>
+    /// Provides a time constrained update source.
+    /// This <see cref="Timer"/> based implementation trades off accuracy for performance.
+    /// Very short intervals (~15ms) may not work as expected.
+    /// See: https://docs.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-5.0#remarks
+    /// If short, accurate intervals are required <see cref="AbstractThreadSleepUpdateSource"/> is the more appropriate choice.
+    /// </summary>
     public abstract class AbstractTimerUpdateSource : AbstractUpdateSource
     {
         private Timer m_Timer;
@@ -24,7 +30,7 @@ namespace Anvil.CSharp.DelayedExecution
 
         private void InitTimer(float updateInterval, TimerCallback callback)
         {
-            int updateIntervalMs = (int)SecondsToMs(updateInterval);
+            int updateIntervalMs = (int)SecondsToMilliseconds(updateInterval);
             WarnIfInvalidInterval(updateIntervalMs);
 
             m_Timer = new Timer(
@@ -47,10 +53,10 @@ namespace Anvil.CSharp.DelayedExecution
             // https://docs.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-5.0#remarks
             if (interval < 15)
             {
-                Log.Warning("Intervals less than 15ms will not work reliably.\n See https://docs.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-5.0#remarks");
+                Log.Warning($"Intervals less than 15ms will not work reliably. Consider using ({nameof(AbstractThreadSleepUpdateSource)})\n See https://docs.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-5.0#remarks");
             }
         }
 
-        private static float SecondsToMs(float seconds) => MathF.Round(seconds * 1000);
+        private static float SecondsToMilliseconds(float seconds) => MathF.Round(seconds * 1000);
     }
 }
