f153b56fac361e7e8eefa722015e36b2618aca4e
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2021-09-03 16:32:56 -0400

    Implement ListExtension.AddUnique

---
+++ Collections/ListExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public static class ListExtension
+{
+    /// <summary>
+    /// Uniquely adds a collection of items to an <see cref="IList{T}"/>.
+    /// Useful for when LINQ can't be used or the underlying list instance must remain the same.
+    /// </summary>
+    /// <param name="list">The list to add to.</param>
+    /// <param name="items">The items to attempt to add.</param>
+    public static void AddUnique<T>(this IList<T> list, IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            if (!list.Contains(item))
+            {
+                list.Add(item);
+            }
+        }
+    }
+}
