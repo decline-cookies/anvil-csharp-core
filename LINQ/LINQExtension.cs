1c7b08539243aabda2b866dfacdfa10f7d339dbf
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2021-10-22 17:21:14 -0400

    LINQ Extension - Add AsCancellable to allow LINQ query cancellation

---
+++ LINQ/LINQExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+public static class LINQExtension
+{
+    public static IEnumerable<TSource> AsCancellable<TSource>(this IEnumerable<TSource> source, CancellationToken token)
+    {
+        foreach (TSource item in source)
+        {
+            if (token.IsCancellationRequested)
+            {
+                token.ThrowIfCancellationRequested();
+                yield break;
+            }
+
+            yield return item;
+        }
+    }
+}
