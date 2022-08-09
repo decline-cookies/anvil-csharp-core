a63a17051960825a3b4ef055f24f0fabf6b635de
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-08-09 12:20:56 -0400

    IDProvider - Allow limit warning to be handled
     - Add formatting to limit warning log message
     - Convert IDLimitWarning events to proper EventHandler<T> types
     - Reduce nesting in CheckIfIDThresholdCrossed

--- Data/IDProvider.cs
+++ Data/IDProvider.cs
@@ -11,11 +11,49 @@ namespace Anvil.CSharp.Data
     public class IDProvider
     {
         /// <summary>
+        /// Arguments that describe an ID limit warning.
+        /// </summary>
+        public class IDLimitWarningEventArgs : EventArgs
+        {
+            /// <summary>
+            /// Indicates whether the limit warning has been handled. If true, no further corrective action is required
+            /// by the application.
+            /// </summary>
+            public bool IsHandled { get; private set; }
+
+            /// <summary>
+            /// Creates a new instance.
+            /// </summary>
+            internal IDLimitWarningEventArgs()
+            {
+                IsHandled = false;
+            }
+
+            /// <summary>
+            /// Call if the limit has been handled and no corrective action is required by the application.
+            /// The event will continue to be raised to handlers for informational purposes (Logging, analytics, etc..)
+            /// </summary>
+            /// <remarks>
+            /// Calling <see cref="Handle"/> multiple times on a single event will raise a warning. While handling the
+            /// limit multiple times probably isn't harmful, it's not recommended.
+            /// </remarks>
+            public void Handle()
+            {
+                if (IsHandled)
+                {
+                    Log.GetLogger(this).Warning($"ID supply limit has already been handled by another listener and shouldn't get handled again.");
+                }
+
+                IsHandled = true;
+            }
+        }
+
+        /// <summary>
         /// Triggers the first time that any instance of <see cref="IDProvider"/> passes its
         /// <see cref="SupplyWarningThreshold"/> for the first time.
         /// This gives the application the opportunity to react before IDs are exhausted.
         /// </summary>
-        public static event Action<IDProvider> OnIDLimitGlobalWarning;
+        public static event EventHandler<IDLimitWarningEventArgs> OnIDLimitGlobalWarning;
 
         /// <summary>
         /// A reserved ID to represent "No ID".
@@ -27,7 +65,7 @@ namespace Anvil.CSharp.Data
         /// Triggers the first time that <see cref="SupplyWarningThreshold"/> is passed.
         /// This gives the consumer the opportunity to react before IDs are exhausted.
         /// </summary>
-        public event Action OnIDLimitWarning;
+        public event EventHandler<IDLimitWarningEventArgs> OnIDLimitWarning;
 
         /// <summary>
         /// The threshold which, when passed, triggers <seealso cref="OnIDLimitWarning"/>.
@@ -75,13 +113,17 @@ namespace Anvil.CSharp.Data
 
         private void CheckIfIDThresholdCrossed()
         {
-            if (m_LastNewID > SupplyWarningThreshold && m_HasIDWarningTriggered)
+            if (m_LastNewID <= SupplyWarningThreshold || m_HasIDWarningTriggered)
             {
-                Log.GetLogger(this).Warning($"{nameof(IDProvider)} has passed its supply warning threshold. Threshold: {SupplyWarningThreshold}");
-                m_HasIDWarningTriggered = true;
-                OnIDLimitWarning?.Invoke();
-                OnIDLimitGlobalWarning?.Invoke(this);
+                return;
             }
+
+            Log.GetLogger(this).Warning($"{nameof(IDProvider)} has passed its supply warning threshold. Threshold: {SupplyWarningThreshold:N0}");
+            m_HasIDWarningTriggered = true;
+
+            IDLimitWarningEventArgs args = new IDLimitWarningEventArgs();
+            OnIDLimitWarning?.Invoke(this, args);
+            OnIDLimitGlobalWarning?.Invoke(this, args);
         }
     }
 }
