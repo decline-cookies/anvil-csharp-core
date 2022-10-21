a7f3192da61a753284bdef683aa3a05f57663092
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-10-21 16:22:58 -0400

    TypeExtension - Now includes outer types and handles inner generic types
     - Improved testing to cover inner generic type handling
     - Updated expected results now that outer types are included in name

--- Reflection/TypeExtension.cs
+++ Reflection/TypeExtension.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Anvil.CSharp.Mathematics;
+using System.Diagnostics;
 
 namespace Anvil.CSharp.Reflection
 {
@@ -48,24 +51,98 @@ namespace Anvil.CSharp.Reflection
         /// </remarks>
         public static string GetReadableName(this Type type)
         {
+            // type.IsGeneric will return true if the type itself or any outer type is generic.
             if (!type.IsGenericType)
             {
-                return type.Name;
+                if (type.DeclaringType == null)
+                {
+                    return type.Name;
+                }
+
+                return $"{type.DeclaringType.GetReadableName()}.{type.Name}";
+            }
+
+            Type[] genericArgs = type.GenericTypeArguments;
+
+            return GetReadableNameOfGenericRecursive(type, genericArgs, genericArgs.Length);
+        }
+
+        /// <summary>
+        /// Traverses up a nested generic type's outer types to build a human readable name.
+        /// </summary>
+        /// <param name="type">The type to get a readable name for.</param>
+        /// <param name="genericArgs">
+        /// The generic arguments of the type ordered from outer to inner generic arguments.
+        /// Ex: MyType<genericArgs[0]>.MyInnerType<genericArgs[1]>
+        /// (The default order returned by <see cref="Type.GenericTypeArguments"/>
+        /// </param>
+        /// <param name="genericArgsUpperBound">
+        /// The upper bounds (exclusive) of the <see cref="genericArgs"/> array to evaluate
+        /// </param>
+        /// <returns></returns>
+        /// <remarks>
+        /// For closed generic types the initial type is the only one with the concrete type args. This is why we pass
+        /// <see cref="genericArgs"/> through each recursion up through the outer types.
+        /// Since we recurse from inner to outer type the <see cref="genericArgsUpperBound"/> tracks the position in
+        /// the array that the current recursion should look to the left of for its generic args.
+        /// </remarks>
+        private static string GetReadableNameOfGenericRecursive(Type type, Type[] genericArgs, int genericArgsUpperBound)
+        {
+            int shallowGenericArgCount = type.GetShallowGenericTypeCount();
+            int genericArgsLowerBound = genericArgsUpperBound - shallowGenericArgCount;
+
+            Debug.Assert(genericArgsUpperBound <= genericArgs.Length);
+            Debug.Assert(genericArgsLowerBound >= 0);
+
+            // The path through any outer types
+            string outerTypes = null;
+            if (type.IsNested)
+            {
+                outerTypes = $"{GetReadableNameOfGenericRecursive(type.DeclaringType, genericArgs, genericArgsLowerBound)}.";
+            }
+
+            // If this type has no generic arguments just use the type name
+            if (shallowGenericArgCount == 0)
+            {
+                return string.Concat(outerTypes, type.Name);
             }
 
             if (type.GetGenericTypeDefinition() == s_NullableType)
             {
-                return $"{type.GenericTypeArguments[0].GetReadableName()}?";
+                return $"{outerTypes}{genericArgs[genericArgsLowerBound].GetReadableName()}?";
             }
 
             // Remove the generic type count indicator (`n) from the type name
             // Avoid having to calculate the number of digits in the generic type count by assuming it's under 100
-            int removeCount = 1 + (type.GenericTypeArguments.Length < 10 ? 1 : 2);
+            int removeCount = 1 + MathUtil.GetDigitCount(shallowGenericArgCount);
             string name = type.Name[..^removeCount];
+            IEnumerable<string> genericTypeArgNames = genericArgs
+                .Skip(genericArgsLowerBound)
+                .Take(shallowGenericArgCount)
+                .Select(arg => arg.GetReadableName());
 
-            string genericTypeNames = string.Join(", ", type.GenericTypeArguments.Select(GetReadableName));
+            return $"{outerTypes}{name}<{string.Join(", ", genericTypeArgNames)}>";
+        }
 
-            return $"{name}<{genericTypeNames}>";
+        /// <summary>
+        /// Gets the number of generic arguments/parameters on a type without counting generic arguments on
+        /// surrounding types.
+        /// </summary>
+        /// <param name="type">The type to evaluate</param>
+        /// <returns>The number of generic arguments for the provided type ignoring any surrounding/outer types</returns>
+        /// <example>typeof(MyType<int>.MyInnerType<float>).GetShallowGenericTypeCount() => 1</example>
+        /// <example>typeof(MyType.MyInnerType<float>).GetShallowGenericTypeCount() => 1</example>
+        /// <example>typeof(MyType<int>.MyInnerType).GetShallowGenericTypeCount() => 0</example>
+        public static int GetShallowGenericTypeCount(this Type type)
+        {
+            if (type.IsNested)
+            {
+                return type.GetGenericArguments().Length - type.DeclaringType.GetGenericArguments().Length;
+            }
+            else
+            {
+                return type.GetGenericArguments().Length;
+            }
         }
     }
 }
--- Tests/Reflection/TypeExtensionTests.cs
+++ Tests/Reflection/TypeExtensionTests.cs
@@ -9,6 +9,36 @@ namespace Anvil.CSharp.Tests
         private class TestGenericClass<T> { }
         private class TestBigGenericClass<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> { }
 
+        private class TestOuterGenericNestedClass<T>
+        {
+            public class TestInnerClass { }
+
+            public class TestInnerGenericClass<InnerT> { }
+        }
+
+        [Test]
+        [Order(1)]
+        public static void GetShallowGenericTypeCount()
+        {
+            Assert.That(typeof(int).GetShallowGenericTypeCount(), Is.EqualTo(0));
+
+            Assert.That(typeof(int?).GetShallowGenericTypeCount(), Is.EqualTo(1));
+
+            Assert.That(typeof(TestClass).GetShallowGenericTypeCount(), Is.EqualTo(0));
+
+            Assert.That(typeof(TestGenericClass<int>).GetShallowGenericTypeCount(), Is.EqualTo(1));
+
+            Assert.That(typeof(TestGenericClass<int?>).GetShallowGenericTypeCount(), Is.EqualTo(1));
+
+            Assert.That(typeof(TestGenericClass<TestGenericClass<TestClass>>).GetShallowGenericTypeCount(), Is.EqualTo(1));
+
+            Assert.That(typeof(TestBigGenericClass<int, int, int, int, int, int, int, int, int, int>).GetShallowGenericTypeCount(), Is.EqualTo(10));
+
+            Assert.That(typeof(TestOuterGenericNestedClass<int>.TestInnerClass).GetShallowGenericTypeCount(), Is.EqualTo(0));
+
+            Assert.That(typeof(TestOuterGenericNestedClass<int>.TestInnerGenericClass<float>).GetShallowGenericTypeCount(), Is.EqualTo(1));
+        }
+
         [Test]
         public static void GetReadableNameTest()
         {
@@ -16,17 +46,34 @@ namespace Anvil.CSharp.Tests
 
             Assert.That(typeof(int?).GetReadableName(), Is.EqualTo("Int32?"));
 
-            Assert.That(typeof(TestClass).GetReadableName(), Is.EqualTo("TestClass"));
+            Assert.That(typeof(TestClass).GetReadableName(), Is.EqualTo("TypeExtensionTests.TestClass"));
 
-            Assert.That(typeof(TestGenericClass<int>).GetReadableName, Is.EqualTo("TestGenericClass<Int32>"));
+            Assert.That(typeof(TestGenericClass<int>).GetReadableName(), Is.EqualTo("TypeExtensionTests.TestGenericClass<Int32>"));
 
-            Assert.That(typeof(TestGenericClass<int?>).GetReadableName, Is.EqualTo("TestGenericClass<Int32?>"));
+            Assert.That(typeof(TestGenericClass<int?>).GetReadableName(), Is.EqualTo("TypeExtensionTests.TestGenericClass<Int32?>"));
 
             Assert.That(typeof(TestGenericClass<TestGenericClass<TestClass>>).GetReadableName(),
-                Is.EqualTo("TestGenericClass<TestGenericClass<TestClass>>"));
+                Is.EqualTo("TypeExtensionTests.TestGenericClass<TypeExtensionTests.TestGenericClass<TypeExtensionTests.TestClass>>"));
 
-            Assert.That(typeof(TestBigGenericClass<int, int, int, int, int, int, int, int, int, int>).GetReadableName,
-                Is.EqualTo("TestBigGenericClass<Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32>"));
+            Assert.That(typeof(TestBigGenericClass<int, int, int, int, int, int, int, int, int, int>).GetReadableName(),
+                Is.EqualTo("TypeExtensionTests.TestBigGenericClass<Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32, Int32>"));
+
+            Assert.That(typeof(TestOuterGenericNestedClass<int>.TestInnerClass).GetReadableName(),
+                Is.EqualTo("TypeExtensionTests.TestOuterGenericNestedClass<Int32>.TestInnerClass"));
+
+            Assert.That(typeof(TestOuterGenericNestedClass<int>.TestInnerGenericClass<float>).GetReadableName(),
+                Is.EqualTo("TypeExtensionTests.TestOuterGenericNestedClass<Int32>.TestInnerGenericClass<Single>"));
+
+            Assert.That(typeof(TestOuterGenericNestedClass<int?>.TestInnerClass).GetReadableName(),
+                Is.EqualTo("TypeExtensionTests.TestOuterGenericNestedClass<Int32?>.TestInnerClass"));
+
+            Assert.That(typeof(TestOuterGenericNestedClass<int?>.TestInnerGenericClass<float?>).GetReadableName(),
+                Is.EqualTo("TypeExtensionTests.TestOuterGenericNestedClass<Int32?>.TestInnerGenericClass<Single?>"));
+
+            Assert.That(typeof(TestOuterGenericNestedClass<>.TestInnerGenericClass<>).GetReadableName(),
+                Is.EqualTo("TypeExtensionTests.TestOuterGenericNestedClass<>.TestInnerGenericClass<>"));
         }
+
+
     }
 }
