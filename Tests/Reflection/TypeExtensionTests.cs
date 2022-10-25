3826d2b93d5824479032e11db4bffff8a30808f2
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-10-25 16:57:10 -0400

    TypeExtensionTests - Implement

--- Tests/Reflection/TypeExtensionTests.cs
+++ Tests/Reflection/TypeExtensionTests.cs
@@ -1,10 +1,36 @@
-
+using NUnit.Framework;
+using Anvil.CSharp.Reflection;
 
 namespace Anvil.CSharp.Tests
 {
     public static class TypeExtensionTests
     {
+        private class TestClass { }
+        private sealed class TestSealedClass { }
+        private static class TestStaticClass { }
+        private abstract class TestAbstractClass { }
+        private struct TestStruct { }
 
+
+        [Test]
+        public static void CreateDefaultValue()
+        {
+            Assert.That(typeof(TestClass).CreateDefaultValue(), Is.EqualTo(null));
+            Assert.That(typeof(TestSealedClass).CreateDefaultValue(), Is.EqualTo(null));
+            Assert.That(typeof(TestStaticClass).CreateDefaultValue(), Is.EqualTo(null));
+            Assert.That(typeof(TestAbstractClass).CreateDefaultValue(), Is.EqualTo(null));
+            Assert.That(typeof(TestStruct).CreateDefaultValue(), Is.EqualTo(default(TestStruct)));
+        }
+
+        [Test]
+        public static void IsStatic()
+        {
+            Assert.That(typeof(TestClass).isStatic(), Is.EqualTo(false));
+            Assert.That(typeof(TestSealedClass).isStatic(), Is.EqualTo(false));
+            Assert.That(typeof(TestStaticClass).isStatic(), Is.EqualTo(true));
+            Assert.That(typeof(TestAbstractClass).isStatic(), Is.EqualTo(false));
+            Assert.That(typeof(TestStruct).isStatic(), Is.EqualTo(false));
+        }
 
 
     }
